26f2efcd3828884c5b81319b7f190eeac926478e
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2021-11-17 21:50:14 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+    $('.menu').on('mouseenter', () => {
+      $('.nav-menu').show();
+    })
+  
+    $('.nav-menu').on('mouseleave', () => {
+      $('.nav-menu').hide();
+    })
+  
+    $('.btn').on('mouseenter', event => {
+      $(event.currentTarget).addClass('btn-hover')
+    }).on('mouseleave', event => {
+      $(event.currentTarget).removeClass('btn-hover')
+    })
+  
+    $('.postText').on('keyup', event => {
+      $('.postText').focus();
+  
+      const post = $(event.currentTarget).val();
+      const remaining = 140 - post.length;
+  
+      $('.characters').html(remaining);
+  
+      if(remaining <= 0){
+        $('.wordcount').addClass('red')
+      } else {
+        $('.wordcount').removeClass('red')
+      }
+    })
+  }); 
+  
